0d524731f0932852710a7ed31d4258b3eabc85ea
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2021-02-04 00:42:03 -0500

    Added C# version

---
+++ src/c-sharp/README.md
@@ -0,0 +1,7 @@
+# Free T-Shirts 👕
+
+Written in C#/C-Sharp.
+
+# How to run
+
+Usually it is run within Visual Studio, or by compiling it to an executable.
---
+++ src/c-sharp/main.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace FreeTShirtsCampaign {
+    class FreeTShirts {
+        static void Main(string[] args) {
+            Console.WriteLine("Free T-Shirts! 👕");
+        }
+    }
+}
